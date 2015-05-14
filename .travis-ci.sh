9e24cc3298738a73d1d18adbe547eaf3e24b8545
John Else <john.else@citrix.com>
2015-05-14 14:20:09 +0100

    Follow redirects when cURLing the diff
    Signed-off-by: John Else <john.else@citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,6 @@
 echo pull req: $TRAVIS_PULL_REQUEST
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  curl https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST.diff -o pullreq.diff
+  curl -L https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST.diff -o pullreq.diff
 else
   git show > pullreq.diff.tmp
   merge=`grep "^Merge: " pullreq.diff.tmp | awk -F: '{print $2}'`
